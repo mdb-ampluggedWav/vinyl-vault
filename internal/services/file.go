406be2317e48e2cc5971153980c17bb11130c311
mdb-ampluggedWav <matdb.music@gmail.com>
2025-10-13 14:12:05 -0400

    1st commit, completion of basic models and auth

--- internal/services/file.go
+++ internal/services/file.go
@@ -1,12 +1,5 @@
 package services
 
-import (
-	"fmt"
-	"mime/multipart"
-	"os"
-	"path/filepath"
-)
-
 type FileUploadResult struct {
 	Path, Filename string
 	Size           uint64
@@ -22,43 +15,4 @@ type FileService struct {
 
 func NewFileService(uploadDir string) *FileService {
 	return &FileService{uploadDir: uploadDir}
-}
-
-// SaveCoverArt saves cover art and returns the path
-func (f *FileService) SaveCoverArt(file *multipart.FileHeader, albumID uint64) (string, error) {
-	// Validate
-	if !f.isValidImage(file) {
-		return "", fmt.Errorf("invalid image format")
-	}
-
-	// Generate path
-	filename := fmt.Sprintf("album_%d_cover%s", albumID, filepath.Ext(file.Filename))
-	fullPath := filepath.Join(f.uploadDir, "covers", filename)
-
-	// Save file
-	// ... implementation
-
-	// Return relative path to store in database
-	return filepath.Join("covers", filename), nil
-}
-
-// SaveAudioFile saves audio and returns the path
-func (f *FileService) SaveAudioFile(file *multipart.FileHeader, trackID uint64) (string, error) {
-	// Similar implementation
-	return "", nil
-}
-
-func (f *FileService) DeleteFile(path string) error {
-	fullPath := filepath.Join(f.uploadDir, path)
-	return os.Remove(fullPath)
-}
-
-func (f *FileService) isValidImage(file *multipart.FileHeader) bool {
-	// Check mime type, extension, etc.
-	return true
-}
-
-func (f *FileService) isValidAudio(file *multipart.FileHeader) bool {
-	// Check mime type, extension, etc.
-	return true
 }
